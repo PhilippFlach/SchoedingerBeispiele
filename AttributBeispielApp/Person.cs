a0755be5984405c9f30e50802f9204345d3324e2
Philipp Flach <flach.philipp@luzi.ch>
2021-07-23 23:15:45 +0200

    PropertyInfo und MethodInfo

---
+++ AttributBeispielApp/Person.cs
@@ -0,0 +1,10 @@
+﻿namespace AttributBeispielApp
+{
+    internal class Person
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Email { get; set; }
+        public string GetFullName() => string.Format($"{this.Firstname}, {this.Lastname}");
+    }
+}
---
+++ AttributBeispielApp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Reflection;
+
+namespace AttributBeispielApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Uebung 1
+            Person schroedi = new Person()
+            {
+                Firstname = "Meister",
+                Lastname = "Schrödinger",
+                Email = "schroedinger@bluewin.ch"
+            };
+
+
+            Type personType = typeof(Person);
+            Console.WriteLine("Type: " + personType.Name);
+            PropertyInfo[] properties = personType.GetProperties();
+
+            foreach (PropertyInfo prop in properties)
+            {
+                Console.WriteLine($"Eigenschaft: {prop.Name} - Wert: {prop.GetValue(schroedi)} ");
+            }
+
+            MethodInfo fullNameMethod = personType.GetMethod("GetFullName");
+
+            Console.WriteLine($"Methode {fullNameMethod?.Name ?? "-"} aufgerufen ergibt: " +
+                $"{fullNameMethod?.Invoke(schroedi, null) ?? "-"}");
+
+            // Uebung 2
+            //GetAuthorInfo(typeof(Person));
+        }
+    }
+}
