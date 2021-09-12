fd32a92b177743a4b264a7777c90c8d75831df02
Philipp Flach <flach.philipp@luzi.ch>
2021-09-12 22:38:05 +0200

    PIBerechnenParallelVarianten mit Seriel und schlechter Parallelisierung

---
+++ SchroedingerParallelPI/PIBerechnenParallelVarianten.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchroedingerParallelPI
+{
+    public static class PIBerechnenParallelVarianten
+    {
+        const int NumSteps = 100000000;
+        public static double SerialPi()
+        {
+            double sum = 0.0;
+            double step = 1.0 / (double)NumSteps;
+            for (int i = 0; i < NumSteps; i++)
+            {
+                double x = (i + 0.5) * step;
+                sum += 4.0 / (1.0 + x * x);
+
+            }
+            return step * sum;
+        }
+
+        /// <summary>
+        /// Diese Variante ist sogar noch schlechter als SerialPi
+        /// </summary>
+        /// <returns></returns>
+        public static double MyParallelPiEinfach()
+        {
+            double sum = 0.0;
+            double step = 1.0 / (double)NumSteps;
+            object monitor = new object();
+            Parallel.For(0, NumSteps, (i) =>
+            {
+                double x = (i + 0.5) * step;
+                double local = 4.0 / (1.0 + x * x);
+                lock (monitor)
+                    sum += local;
+            });
+            return step * sum;
+        }
+    }
+}
---
+++ SchroedingerParallelPITests/PIBerechnenParallelVariantenTests.cs
@@ -0,0 +1,60 @@
+﻿using NUnit.Framework;
+using SchroedingerParallelPI;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchroedingerParallelPI.Tests
+{
+    [TestFixture()]
+    public class PIBerechnenParallelVariantenTests
+    {
+        const double PIApprox = 3.14159265;
+        private Stopwatch _stopWatch;
+
+
+        [SetUp]
+        public void Init()
+        {
+            _stopWatch = Stopwatch.StartNew();
+        }
+
+        [TearDown]
+        public void Cleanup()
+        {
+            _stopWatch.Stop();
+            TestContext.WriteLine("Excution time for {0} - {1} ms",
+            TestContext.CurrentContext.Test.Name,
+            _stopWatch.ElapsedMilliseconds);
+        }
+
+        [Test()]
+        public void SerialPiTest()
+        {
+            // Arrange
+            double PIBerechnet;
+
+            //Act
+            PIBerechnet = PIBerechnenParallelVarianten.SerialPi();
+
+            //Assert
+            Assert.AreEqual(PIApprox, PIBerechnet, 0.00005);
+        }
+
+        [Test()]
+        public void MyParallelPiEinfachTest()
+        {
+            // Arrange
+            double PIBerechnet;
+
+            //Act
+            PIBerechnet = PIBerechnenParallelVarianten.MyParallelPiEinfach();
+
+            //Assert
+            Assert.AreEqual(PIApprox, PIBerechnet, 0.00005);
+        }
+    }
+}
