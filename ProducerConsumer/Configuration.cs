a9241adf26326816b0eba01fd09cd1f323ecb1d2
Philipp Flach <philipp.flach@luzi.com>
2022-08-21 00:36:27 +0200

    Start bis Seite 535 Mitte

---
+++ ProducerConsumer/Configuration.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProducerConsumer
+{
+    public static class Configuration
+    {
+        // ein paar Variablen und Objekte um das Programm steuern zu können
+        static int amountOfToiletts = 4;
+        static int amountOfPerson = 200;
+        /* 
+         * BlockingCollection sperrt den Zugriff von einzelnen Theads.
+         * Es ist für unseren Anwendungsfall perfekt, da es den Produzenten blockiert,
+         * wenn alle Toiletten besetzt sind, aber den oder die 
+         * Konsumenten, wenn keiner die Toiletten benutzt.
+         * Die Anzahl der verfügbaren Toiletten ergibt auch die Grösse 
+         * der BlockingCollection
+         */
+        
+        static BlockingCollection<Person> toiletts = new BlockingCollection<Person>(amountOfToiletts);
+        /* Für die Simulation
+         */
+        static Random rnd = new Random(DateTime.Now.Millisecond);
+        private static Person[] InitializePersons()
+        {
+            Person[] persons = new Person[amountOfPerson];
+            for (int i = 0; i < amountOfPerson; i++)
+            {
+                // Die Personen werden mit zufälligen unterschiedlichen Toilettenzeiten belegt
+                persons[i] = new Person(rnd.Next(2, 30), rnd.Next(80, 250));
+            }
+            return persons;
+        }
+
+    }
+}
---
+++ ProducerConsumer/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProducerConsumer
+{
+    public class Person
+    {
+        /// <summary>
+        /// Person ist ein Produzent von Urin
+        /// Wenn die Person auf dem WC sitzt, braucht sie Zeit 
+        /// und dann ist sie der Konsument
+        /// </summary>
+        /// <param name="timeOnToilette">Gibt die Zeit an, die die Person auf der Toilette verbringen wird</param>
+        /// <param name="waitUntilToilette">Gibt die maximale Zeit an, die die Person aushält, bis sie auf die Toilette muss</param>
+        public Person(int timeOnToilette, int waitUntilToilette)
+        {
+            TimeOnToilette = timeOnToilette;
+            WaitUntilToilette = waitUntilToilette;
+        }
+
+        public int TimeOnToilette { get; }
+        public int WaitUntilToilette { get; }
+    }
+}
